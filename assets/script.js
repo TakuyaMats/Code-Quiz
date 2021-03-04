59b156d97a475c1155c1291b20995ea02153eaa2
TMats <takomats@hotmail.com>
2021-03-03 22:46:39 -0500

    Question Prompt added

--- assets/script.js
+++ assets/script.js
@@ -13,10 +13,13 @@ var startQuiz = document.getElementById('start-button');
 var submitInitials = document.getElementById('submit-initials');
 var questionsDiv = document.getElementById('questionsDiv');
 var createUl = document.createElement("ul");
+var userQuestions = document.getElementById("questions");
+var userAnswers = document.getElementById("answers");
 
 var count = 60;
 var score = 0;
 var questionIndex = 0;
+var currentQuestions = 0;
 
 var questions = [{
         title: "What is the correct definition of an HTML element?",
@@ -31,19 +34,21 @@ var questions = [{
     }
 ]
 
-// render the question prompt
-function generateQuiz(questionIndex) {
-    questionsDiv.innerHTML = "";
-    createUl.innerHTML = "";
-
-    for (var i = 0; i < questions.length; i++) {
-        var userQuestion = questions[questionIndex].title;
-        var userChoices = questions[questionIndex].choices;
-        questionsDiv.textContent = userQuestion;
-    }
-}
-
 startQuiz.addEventListener("click", function () {
+    
+    function getNextQuestion(userChoice) {
+        let userQuestions = questions[currentQuestions].answer; 
+
+            if (userChoice === userQuestions) {
+                console.log("Correct!");
+                score++;
+            } else {
+                console.log("Wrong Answer!")
+            }
+        currentQuestions++;
+        getNextQuestion(currentQuestions);
+    }
+
     var interval = setInterval(function () {
         timeRemaining.innerHTML = count;
         count--;
@@ -53,34 +58,31 @@ startQuiz.addEventListener("click", function () {
         }
     }, 5000);
     console.log(interval);
+    console.log(getNextQuestion())
+    return getNextQuestion;
 })
 
-var currentQuestions = 0;
-function getNextQuestion (userChoice) {
-    var userQuestions = question[currentQuestions].answer; {
-    
-        if (userChoice===userQuestions) {
-            console.log("Correct!")
-        }
-        else {
-            console.log("Wrong Answer!")
-        }
-        getNextQuestion++
-    }
-
-
-}
-
-var userQuestions = document.getElementById("questions");
-var userAnswers = document.getElementById("answers");
 function answersClicks() {
     var a = document.getElementById("answer1");
     var b = document.getElementById("answer2");
     var c = document.getElementById("answer3");
     var d = document.getElementById("answer4");
 
-    a.addEventListener("click", function());
-    b.addEventListener("click", function());
-    c.addEventListener("click", function());
-    d.addEventListener("click", function());
+    a.addEventListener("click", function (){ getNextQuestion(a.innerText); });
+    b.addEventListener("click", function (){ getNextQuestion(b.innerText); });
+    c.addEventListener("click", function (){ getNextQuestion(c.innerText); });
+    d.addEventListener("click", function (){ getNextQuestion(d.innerText); });
 }
+answersClicks();
+
+// // render the question prompt
+// function generateQuiz(questionIndex) {
+//     questionsDiv.innerHTML = "";
+//     createUl.innerHTML = "";
+
+//     for (var i = 0; i < questions.length; i++) {
+//         var userQuestion = questions[questionIndex].title;
+//         var userChoice = questions[questionIndex].choices;
+//         questionsDiv.textContent = userQuestion;
+//     }
+// }
