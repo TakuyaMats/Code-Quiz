35eb95e27bd0bf022e9d902a05b8ebe2dc384b21
TMats <takomats@hotmail.com>
2021-03-05 17:28:23 -0500

    Added placeholder javascript code just to see how a logic is working

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,75 @@
-var highScoreElement = document.getElementById('highScore');
-var timerDisplay = document.getElementById('time-remaining');
-var landingPage = document.getElementById('landing-container');
-var startQuiz = document.getElementById('start-button');
-var submitInitials = document.getElementById('submit-initials');
-var createUl = document.createElement("ul");
-var questionsPrompt = document.getElementById('question-prompt');
-var questionsElement = document.getElementById('question');
-var answerElement1 = document.getElementById('answer1');
-var answerElement2 = document.getElementById('answer2');
-var answerElement3 = document.getElementById('answer3');
-var answerElement4 = document.getElementById('answer4');
+const highScoreElement = document.getElementById('highScore');
+const timerDisplay = document.getElementById('time-remaining');
+const landingPage = document.getElementById('landing-container');
+const startQuiz = document.getElementById('start-button');
+const submitInitials = document.getElementById('submit-initials');
+const createUl = document.createElement("ul");
+const questionsPrompt = document.getElementById('question-prompt');
+const questionsElement = document.getElementById('question');
+const progressText = document.querySelector('#progressText')
+const choices = Array.from(document.querySelectorAll('.choice-text'));
 
-var secondsLeft = 120;
-var highScore = 0;
-var questionIndex = 0;
-var currentQuestions = 0;
+let secondsLeft = 120;
+let highScore = 0;
+let scorePoints = 100;
+let maxQuestions = 5;
+let questionIndex = 0;
+let myTimer;
+let currentQuestion = {};
 underScoreArray = [];
-var myTimer;
 
-var questions = [{
-    question: "What is the correct definition of an HTML element?",
-    choices: ["The second, or closing, HTML tag. Closing tags have a forward slash (/) inside them.", "HTML code that does not require opening or closing tags.", "An HTML tag and the content that it contains or marks up.", "The first, opening, HTML tag"],
-    answer: "An HTML tag and the content that it contains or marks up."
-}, ]
+let questions = [
+    {
+        question: "What is the correct definition of an HTML element?",
+        choice1: "The second, or closing, HTML tag. Closing tags have a forward slash (/) inside them.",
+        choice2: "HTML code that does not require opening or closing tags.",
+        choice3: "An HTML tag and the content that it contains or marks up.",
+        choice4: "The first, opening, HTML tag",
+        answer: 3,
+    },
+    {
+        question: "What's the difference between <embed> and <video>?",
+        choice1: "<video> can only be used for video, while <embed> can be used for any type of media.",
+        choice2: "<embed> does not support video while <video> does.",
+        choice3: "<embed> is used in the header of the document while <video> can be used anywhere.",
+        choice4: "<video> is semantic element while <embed> is not.",
+        answer: 1,
+    },
+    {
+        question: "Why is the <div> tag not semantic?",
+        choice1: "It requires a closing tag.",
+        choice2: "It provides context as to what content is inside of the tag.",
+        choice3: "It provides no context as to what the content is inside the tag.",
+        choice4: "It can be used multiple times throughout the code.",
+        answer: 3,
+    },
+    {
+        question: "What is the purpose of indentation and whitespace?",
+        choice1: "To comment out code you don't need.",
+        choice2: "To make code in an HTML file more readable",
+        choice3: "To change the display of elements in the browser.",
+        choice4: "To make sure the browser correctly interprets nested elements in an HTML file.",
+        answer: 2,
+    },
+    {
+        question: "Why should the document type declaration be included in all HTML documents?",
+        choice1: "To specify what content will display in the browser.",
+        choice2: "Without it, the web page will not be allowed on the internet.",
+        choice3: "To specify the HTML standard being used in the content.",
+        choice4: "Without it, any HTML code is invalid.",
+        answer: 3,
+    }
+]
 
 startQuiz.addEventListener("click", function() {
 
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+
     function startGame() {
         underScoreArray = [];
-        var secondsLeft = 120;
+        let secondsLeft = 120;
         timerDisplay.textContent = secondsLeft; 
         }
         myTimer = setInterval(alertTime, 1000);
@@ -41,6 +82,7 @@ startQuiz.addEventListener("click", function() {
         clearInterval(myTimer); 
     }
     startGame()
+    getNewQuestion();
 });
 
 
@@ -48,6 +90,65 @@ startQuiz.addEventListener("click", function() {
 
 
 
-function showQuestions() {
-    document.getElementById('questionsPrompt').innerHTML = questions[0].title;
+
+
+
+
+
+
+
+// Placeholder code to see how to get the questions to show up for the user.
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter > maxQuestions) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${maxQuestions}`
+    progressBarFull.style.width = `${(questionCounter/maxQuestions) * 100}%`
+    
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
 }
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(scorePoints)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
+// Placeholder code to see how to get the questions to show up for the user.
